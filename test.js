b2b709c1356e0a2ace50d4dadb99dccf89de28cd
ekkertigor <ekkert.igor@gmail.com>
2016-09-09 00:59:25 +0300

    Plular function and counting emails

---
+++ test.js
@@ -0,0 +1,11 @@
+let assert = require('assert');
+let hello = require('./public/main').hello;
+let plural = require('./public/main').plural;
+
+assert.equal(plural(0), 'раз');
+assert.equal(plural(1), 'раз');
+assert.equal(plural(2), 'раза');
+assert.equal(plural(13), 'раз');
+assert.equal(plural(15), 'раз');
+assert.equal(plural(22), 'раза');
+assert.equal(plural(100), 'раз');
