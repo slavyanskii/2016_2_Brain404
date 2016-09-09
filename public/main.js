aff44c04e27633e2ceccc86ab5781024d47e395c
ekkertigor <ekkert.igor@gmail.com>
2016-09-09 17:11:07 +0300

    edit gitignore, work version

--- public/main.js
+++ public/main.js
@@ -16,7 +16,7 @@ function hello (text, number) {
 }
 
 function plural(number) {
-    if ((number % 10 === 2 || number % 10 === 3 || number % 10 === 4) && (number < 12 || number > 14)) {
+    if ((number % 10 === (2 || 3 || 4)) && (number < 12 || number > 14)) {
         return ('раза');
     }
     else {
